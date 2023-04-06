879306a6b3acfa2b9e3c470d9c4dba3a4dfd1df8
ValintaRS <valintars@gmail.com>
2023-04-06 10:23:44 +0300

    2/Test without dist, added start script

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss'
 
-// tell webpack to add index.html and appconfig.json to output
-require("!file-loader?name=[name].[ext]!./index.html");
-require("!file-loader?name=[name].[ext]!./appconfig.json");
+// // tell webpack to add index.html and appconfig.json to output
+// require("!file-loader?name=[name].[ext]!./index.html");
+// require("!file-loader?name=[name].[ext]!./appconfig.json");
 
 // check if we are running inside alt1 by checking if the alt1 global exists
 if (window.alt1) {
 	// tell alt1 about the app
 	// this makes alt1 show the add app button when running inside the embedded browser
 	// also updates app settings if they are changed
-	alt1.identifyAppUrl("appconfig.json");
+	alt1.identifyAppUrl("../../appconfig.json");
 }
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
